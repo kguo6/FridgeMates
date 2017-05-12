383d7e05ec2fc68037f9ada47bc5532b914ac701
Vincent Lam <vincentlam@vlam-MBP.local>
2017-05-12 16:39:53 -0700

    added login button hover css

--- frontend/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$f = fopen('books.txt', 'w') or die('error');
-
-fwrite($f, "getting to yes\n". PHP_EOL);
-fwrite($f, 'four hour workweek\n' . PHP_EOL);
-fwrite($f, "getting real".PHP_EOL);
-
-fclose($f);
