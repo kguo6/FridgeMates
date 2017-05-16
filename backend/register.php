0a95b4483017b73e8805a8cd397808850a782478
Wayne Huang <wayne.huang.21@gmail.com>
2017-05-15 17:21:58 -0700

    fixed some validation, added error notification using ajax, better login/registration navigation

--- backend/register.php
+++ backend/register.php
@@ -3,32 +3,32 @@
     
     require_once("config.php");
 
-    $email = $_POST["register_username"];
-    $password = $_POST["register_password"];
-    $name = $_POST["register_name"];
+    if($_POST) {
+        $email = trim($_POST["register_username"]);
+        $password = trim($_POST["register_password"]);
+        $name = trim($_POST["register_name"]);
 
-    $query1="SELECT user_id FROM Users
-             WHERE email = \"".$email."\"";
-    $query2="INSERT INTO Users (user_id, email, password, name)
-            VALUES (NULL,?,?,?)";
-    
-    $response = @mysqli_query($dbc, $query1);
-    $row = mysqli_fetch_array($response);
-    $stmt = @mysqli_prepare($dbc, $query2);
+        $query1="SELECT user_id FROM Users
+                WHERE email = \"".$email."\"";
+        $query2="INSERT INTO Users (user_id, email, password, name)
+                VALUES (NULL,?,?,?)";
+        
+        $response = @mysqli_query($dbc, $query1);
+        $row = mysqli_fetch_array($response);
+        $stmt = @mysqli_prepare($dbc, $query2);
 
-    if(isset($row)) {
-        $_SESSION['ERROR'] = 2;
+        if(isset($row)) {
+            echo 1;
+        }
+        else {
+            mysqli_stmt_bind_param($stmt, "sss", $email, md5($password), $name);
+        
+            mysqli_stmt_execute($stmt);
+                
+            echo 2;
+        }
+
+        mysqli_close($dbc);
     }
-    else {
-        mysqli_stmt_bind_param($stmt, "sss", $email, md5($password), $name);
-	
-    	mysqli_stmt_execute($stmt);
-			
-        $_SESSION['ERROR'] = 3;
-    }
-
-    mysqli_close($dbc);
-    header("location: index.php");
-
 
 ?>
