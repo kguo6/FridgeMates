bd2f9cb73f9c519abe0e1288c8b2e3235d5bfb43
Wayne Huang <wayne.huang.21@gmail.com>
2017-05-18 13:58:49 -0700

    fixed up about page. all pages for sprint 2 done, but myfridge page css needs cleaning up and commeting, as well as index pages.

--- v1.0/about.php
+++ v1.0/about.php
@@ -5,7 +5,7 @@
 <!doctype html>
 <html lang="en">
 <head>
-<title>FridgeMates-About US Page</title>
+<title>FridgeMates-Home</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -18,6 +18,17 @@
 <script src="js/register-validate.js"></script>
 <script src="js/login-validate.js"></script>
 <script>
+    //function to load content of page on body load
+    function content() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if(this.readyState == 4 && this.status == 200) {
+                document.getElementById("content").innerHTML = this.responseText;
+            }
+        };
+        xhttp.open("GET", "aboutus.php", true);
+        xhttp.send();
+    }
     //load register form on login modal
     function register() {
         document.getElementById("loginContainer").style.display ="none";
@@ -36,7 +47,7 @@
     }
 </script>
 </head>
-<body data-spy="scroll" data-target=".navbar" data-offset="10" onload="login();">
+<body data-spy="scroll" data-target=".navbar" data-offset="10" onload="login();content();">
     <!-- navigation bar -->
     <nav class="navbar navbar-default navbar-fixed-top" id="header_nav">
         <div class="container-fluid">
@@ -115,58 +126,6 @@
     </div>
     <!-- wrapper for content of the page -->
     <div id="content" class="page-wrap">
-        <div class="jumbotron text-center">
-            <h1 class="underline"><span>Welcome</span></h1>
-            <h2>We are team 30, a group of cs students that<br> share the passions for preserving the environment</h2>
-        </div>
-
-        <div class="container-fluid bg1" style="background-color: #C7D3C8;  padding:40px">
-			<div class="col-sm-16">
-			<p class="smalltxt"><i>“Imagine walking out of a grocery store with four bags of groceries, dropping one in the parking lot, and just not bothering to pick it up. That’s essentially what we’re doing.”</i> - Dana Gunders </p>
-			</div>
-			<p class="largetxt">People in a community (i.e. offices, families, dormitories, etc.) are unaware of what food items are available and what can be eaten in a shared fridge. FridgeMates is a web app that keeps track of users, the user’s leftover food, and the amount of time each food item has been in the fridge. Our goal is to  improve society's behavior toward food waste and awareness towards preserving the environment.
-        </div>
-        
-        <div class="container-fluid bg2" style="background-color: #3BB245; padding-bottom: 40px">
-            <h2 style="display:inline-block; border-bottom:3px solid #3BB245;">Meet the Team</h2>
-    <div class="row">
-        <div class="col-sm-4">
-				<div class="icon">
-					<img src="http://img0.liveinternet.ru/images/attach/c/8/100/933/100933038_large_tumblr_mlfc88lgS61roihxco5_250.png" alt="Kevin1">
-				<h3 style="text-align:center">Ryan Mar</h3>
-				</div>
-				<p style="text-align:center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-			</div>
-        <div class="col-sm-4">
-				<div class="icon">
-					<img src="http://img1.liveinternet.ru/images/attach/c/8/100/933/100933041_large_tumblr_mlfc88lgS61roihxco8_250.png" alt="Kevin2">
-					<h3 style="text-align:center">Kenny Guo</h3>
-				</div>
-				<p style="text-align:center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-			</div>
-        <div class="col-sm-4">
-				<div class="icon">
-					<img src="http://img1.liveinternet.ru/images/attach/c/8/100/933/100933037_large_tumblr_mlfc88lgS61roihxco4_250.png" alt="Kevin3">
-					<h3 style="text-align:center">Wayne Huang</h3>
-				</div>
-				<p style="text-align:center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-			</div>
-        <div class="col-sm-4">
-				<div class="icon">
-					<img src="http://img1.liveinternet.ru/images/attach/c/8/100/933/100933035_large_tumblr_mlfc88lgS61roihxco1_250.png" alt="Kevin4">
-					<h3 style="text-align:center; border-bottom:3px solid #3BB245">Vincent Lam</h3>
-				</div>
-				<p style="text-align:center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-			</div>
-        <div class="col-sm-4">
-				<div class="icon">
-					<img src="http://img0.liveinternet.ru/images/attach/c/8/100/933/100933040_large_tumblr_mlfc88lgS61roihxco7_250.png" alt="Kevin5">
-					<h3 style="text-align:center">Shang Li</h3>
-				</div>
-				<p style="text-align:center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-			</div>
-		</div>
-        </div>
     </div>
     <!-- footer -->
     <footer>
