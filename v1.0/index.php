e0ee1b5b7f335fa17a3e0d7f4ef842fec35785d4
Kenny <kennyguoyuchen@hotmail.com>
2017-05-18 09:23:48 -0700

    readding indexphp

---
+++ v1.0/index.php
@@ -0,0 +1,178 @@
+<?php
+    session_start();
+?>
+<!doctype html>
+<html lang="en">
+<head>
+<title>FridgeMates-Home</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="css/loginmodal.css">
+<link rel="stylesheet" href="css/headerfooter.css">
+<link rel="stylesheet" type="text/css" href="css/makeItRain.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.1/jquery.validate.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="js/register-validate.js"></script>
+<script src="js/makeItRain.js"></script>
+<script>
+    //index page function to load pre/post login homepage
+    function content() {
+        <?php
+            if(isset($_SESSION['USER_ID']) &&
+	          (trim($_SESSION['USER_ID']) != '')) {
+                  echo 'var xhttp = new XMLHttpRequest();
+                        xhttp.onreadystatechange = function() {
+                            if(this.readyState == 4 && this.status == 200) {
+                                document.getElementById("content").innerHTML = this.responseText;
+                            }
+                        };
+                        xhttp.open("GET", "welcome.php", true);
+                        xhttp.send();';
+            }
+            else {
+                echo 'var xhttp = new XMLHttpRequest();
+                      xhttp.onreadystatechange = function() {
+                          if(this.readyState == 4 && this.status == 200) {
+                              document.getElementById("content").innerHTML = this.responseText;
+                          }
+                      };
+                      xhttp.open("GET", "loginform.php", true);
+                      xhttp.send();';
+            }
+        ?>
+    }
+    //load register form on login modal
+    function register() {
+        document.getElementById("loginContainer").style.display ="none";
+        document.getElementById("registerContainer").style.display ="block";
+    }
+    //load login form on login modal
+    function login() {
+        document.getElementById("registerContainer").style.display ="none";
+        document.getElementById("loginContainer").style.display ="block";
+    }
+    //check for alerts
+    function alerts() {
+        var error_num = <?php echo $_SESSION['ERROR'] ?>;
+        if(error_num == 1) {
+            alert("Login Error");
+        }
+        if(error_num == 2) {
+            alert("User Already Exists");
+        }
+        if(error_num == 3) {
+            alert("Registration Successful");
+        }
+        <?php $_SESSION['ERROR'] = 0; ?>
+    }
+
+
+    // counter for clicks
+    // invokes js when click condition is met
+    var counter=0;
+    $(document).ready(function(){
+        $("#click").on(
+            "click",function(){
+                 if(counter == 4) {
+                     $(this).makeItRain();
+                 }
+                 else {
+                     counter++;
+                 }
+        });
+    });
+</script>
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="10" onload="content();login();alerts();">
+    <?php
+        //check for alert variable and sets it to 0 if none
+        if($_SESSION['ERROR'] != 1 || $_SESSION['ERROR'] != 2 || $_SESSION['ERROR'] != 3) {
+            $_SESSION['ERROR'] = 0;
+        }
+    ?>
+    <!-- navigation bar -->
+    <nav class="navbar navbar-default navbar-fixed-top" id="header_nav">
+        <div class="container-fluid">
+            <div class="navbar-header pull-left">
+                <img src="logo/logoblue.png" class="logo" id="nav_logo">
+            </div>
+            <div class="navbar-header navbar-right">
+                <button type="button" id="nav_toggle" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span> 
+                </button>
+                <?php
+                    if(isset($_SESSION['USER_ID']) &&
+                    (trim($_SESSION['USER_ID']) != '')) {
+                        echo '<div class="btn-group" id="account_group"><button type="button" class="btn" id="login_btn" dropdown-toggle" data-toggle="dropdown" style="margin-left:25px">'.$_SESSION["USER_NAME"].'</button>
+                                <ul class="dropdown-menu" id="account_dropdown">
+                                    <li><a href="account.php">ACCOUNT</a></li>
+                                    <li><a href="logout.php">LOGOUT</a></li>
+                                </ul></div>';
+                    }
+                    else {
+                        echo '<button type="button" class="btn" id="login_btn" data-toggle="modal" data-target="#login-modal" onclick="login()">Log In</button>';
+                    }
+                ?>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right" id="nav_list">
+                    <li><a href="index.php">HOME</a></li>
+                    <?php
+                        if(isset($_SESSION['USER_ID']) &&
+                        (trim($_SESSION['USER_ID']) != '')) {
+                            echo '<li><a href="fridge.php">MY FRIDGES</a></li>';
+                        }
+                    ?>
+                    <li><a href="affiliate.php">AFFILIATES</a></li>
+                    <li><a href="support.php">SUPPORT</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- login/register modal -->
+    <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
+        <div class="modal-dialog">
+            <div class="loginmodal-container" id="loginmodal-container">
+                <div id="registerContainer">
+                    <h1>Register a New Account</h1><br>
+                    <form  action="register.php" method="POST" id="registerForm">
+                        <input type="email" name="register_username" placeholder="Email">
+                        <input type="password" id="register_password" name="register_password" placeholder="Password (Min. 6 Characters)">
+                        <input type="password" name="confirm_password" placeholder="Confirm Password">
+                        <input type="text" name="register_name" placeholder="Username (Min. 4 Characters)">
+                        <input type="submit" name="login" class="login loginmodal-submit" id="register_btn" value="Register">
+                    </form>
+                                        
+                    <div class="login-help">
+                        <a href="#" onclick="login()">Login</a>
+                    </div>
+                </div>
+                <div id="loginContainer">
+                    <h1>Login to Your Account</h1><br>
+                    <form action="login.php" method="POST" id="loginForm">
+                        <input type="email" name="login_username" placeholder="Email">
+                        <input type="password" name="login_password" placeholder="Password">
+                        <input type="submit" name="login" class="login loginmodal-submit" value="Login">
+                    </form>
+                                        
+                    <div class="login-help">
+                        <a href="#" id="registerLink" onclick="register()">Register</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- wrapper for content of the page -->
+    <div id="content" class="page-wrap">
+    </div>
+    <!-- footer -->
+    <footer>
+        <a href="about.php">About Us</a> | <a href="sitemap.php">Site Map</a>
+        <p>Copyright <span id="click">&copy;</span> 2017 FridgeMates </p>
+    </footer>
+</body>
+</html>
