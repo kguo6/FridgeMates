d59a5002511610c9096305daa6f943ee67c95dc7
Kenny <kennyguoyuchen@hotmail.com>
2017-05-18 22:38:13 -0700

    LOL

--- v1.0/js/makeItRain.js
+++ v1.0/js/makeItRain.js
@@ -3,7 +3,7 @@ $.fn.makeItRain = function(){
 		$(this).on('click',function(){
 
 		    var maxFood = 10;
-			var maxBills = 30;
+			var maxBills = 20;
 
 			if(counter == 4) {
 
@@ -14,21 +14,23 @@ $.fn.makeItRain = function(){
                     var randomPosition = Math.floor((random - 150) * Math.random());
                     var randomTime = Math.random() * 10;
                     var randomSpeed = (Math.random() * 10) + 10;
-                    if (i < maxFood) {
-                        var chicken = $("<span class='foodFoodFood'>")
+                   // if (i < maxFood) {
+                        var chicken = $("<span class='billsBillsBills'>")
                             .css({
                                 left: randomPosition,
                                 top: '-150px',
-                                "-webkit-animation-delay": randomTime + 2 + "s",
-                                "-webkit-animation-duration": 7 + "s"
+                                "-webkit-animation-delay": randomTime + "s",
+                                "-webkit-animation-duration": randomSpeed + "s"
                             });
-                        $(chicken).prepend('<img class = fallingstuff style ="width:80px;"src="images/chris.svg" alt="leftover chicken">');
+                        $(chicken).prepend('<img class = fallingstuff style ="width:40px;"src="images/chris.png" alt="leftover chicken">');
 
                         $('body').append(chicken);
 
-                    }
+                   // }
 
-
+                    randomPosition = Math.floor((random - 150) * Math.random());
+                    randomTime = Math.random() * 10;
+                    randomSpeed = (Math.random() * 10) + 10;
                     var bills = $("<span class='billsBillsBills'>")
                         .css({
                             left: randomPosition,
@@ -38,7 +40,7 @@ $.fn.makeItRain = function(){
 
                         });
 
-                    $(bills).prepend('<img class = fallingstuff style ="width:80px;"src="images/carly.svg" alt="a dollar bill">');
+                    $(bills).prepend('<img class = fallingstuff style ="width:40px;"src="images/carly.png" alt="a dollar bill">');
 
 
                     $('body').append(bills);
